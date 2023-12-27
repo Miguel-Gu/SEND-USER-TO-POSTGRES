0f4608e525fde7b012c513495a8669aad49f3804
Miguel-Gu <gumiguel8@hotmail.com>
2023-12-26 22:31:19 -0300

    feat: check if user already exists

--- app/main.py
+++ app/main.py
@@ -4,22 +4,32 @@ from fastapi.logger import logger
 from sqlalchemy.orm import Session
 from app.sender import insert_usuario
 from app.database import get_db
-from app.models import UsuarioEntrada
+from app.models import UsuarioEntrada, Usuario
 
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "ok"}
 
+
 @app.post("/novousuario")
 def post_usuario(payload: UsuarioEntrada, db: Session = Depends(get_db)):
     try:
-        insert_usuario(payload, db)
-    
+        query = db.query(Usuario).filter(
+            Usuario.email == payload.usuario.email).first()
+        
+        if not query:
+            insert_usuario(payload, db)
+
+        else:
+            return JSONResponse(status_code=status.HTTP_409_CONFLICT, content="User already exists")
+
+
     except Exception as error:
         logger.warning("Error when sending data to postgres")
         logger.warning(f"error - {error}")
-    
+
     return JSONResponse(status_code=status.HTTP_200_OK, content="Data send with sucess")
